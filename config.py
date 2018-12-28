7705134e42d7f63e81ec47624fed97410744b070
Zhao Qiang <qiang.zhao@nxp.com>
2018-12-28 10:23:06 +0800

    add regist funtion
    Signed-off-by: Zhao Qiang <qiang.zhao@nxp.com>

--- config.py
+++ config.py
@@ -11,3 +11,5 @@ USERNAME = 'root'
 PASSWORD = 'root'
 DB_URI = 'mysql+mysqldb://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
