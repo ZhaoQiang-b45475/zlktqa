66d34494e2efca1234abe71168b48347be1f7c5d
Zhao Qiang <qiang.zhao@nxp.com>
2019-01-03 10:28:55 +0800

    add question page and function
    Signed-off-by: Zhao Qiang <qiang.zhao@nxp.com>

--- zlktqa.py
+++ zlktqa.py
@@ -57,6 +57,13 @@ def logout():
     session.clear()
     return redirect(url_for("login"))
 
+@app.route('/question/', methods=["GET", "POST"])
+def question():
+    if request.method == "GET":
+        return render_template("question.html")
+    else:
+        pass
+
 @app.context_processor
 def my_context_processor():
     user_id = session.get('user_id')
