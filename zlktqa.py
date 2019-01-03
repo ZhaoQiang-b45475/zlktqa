4c7b1a3b8df2a31f4c78b97d2b48c213959f60fe
Zhao Qiang <qiang.zhao@nxp.com>
2019-01-03 16:15:30 +0800

    add homepage function
    Signed-off-by: Zhao Qiang <qiang.zhao@nxp.com>

--- zlktqa.py
+++ zlktqa.py
@@ -2,12 +2,14 @@
 
 from flask import Flask, render_template, request, redirect, url_for, session
 import config
-from models import User
+from models import User, Question
 from exts import db
+from decorators import login_required
 
 app = Flask(__name__)
 app.config.from_object(config)
 db.init_app(app)
+
 
 @app.route('/')
 def index():
@@ -58,11 +60,20 @@ def logout():
     return redirect(url_for("login"))
 
 @app.route('/question/', methods=["GET", "POST"])
+@login_required
 def question():
     if request.method == "GET":
         return render_template("question.html")
     else:
-        pass
+        title = request.form.get("title")
+        content = request.form.get("content")
+        question = Question(title = title, content=content)
+        user_id = session.get('user_id')
+        user = User.query.filter(User.id == user_id).first()
+        question.author = user
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for("index"))
 
 @app.context_processor
 def my_context_processor():
